5a82227b9dfc1c2c8d9e471d52d8933868400eed
AGRAWALrishabh11 <rishabh.agrawal1102@gmail.com>
2024-10-21 09:15:42 +0530

    hello

--- Day6/script.js
+++ Day6/script.js
@@ -7,3 +7,16 @@ let arr=[1,2,3,4,5,6];
 
 let sum=even.reduce((x,y)=>x+y,0);
 console.log(sum);
+
+const students=[
+    {name: 'Alice' ,score:50},
+    {name: 'Bob' ,score:70},
+    {name: 'charlie' ,score:40},
+    {name: 'David' ,score:75},
+];
+
+let total=students.filter((student)=> student.score >60)
+.map((student)=>student.score*2)
+.reduce((x,y)=>x+y,0);
+
+console.log(total);
